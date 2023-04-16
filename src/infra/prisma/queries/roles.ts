e26940246a8253bb8384011b4b02c7fe3af36c2e
Gustavo Carvalho <gcrodrigues413@gmail.com>
2023-04-16 03:22:58 -0300

    feat: check useer roles

---
+++ src/infra/prisma/queries/roles.ts
@@ -0,0 +1,28 @@
+
+import { PrismaClient, Roles } from '@prisma/client';
+import { inject, injectable } from 'tsyringe';
+import IRolesRepository from '../../../domain/roles/repositories/rolesRepository';
+
+@injectable()
+class RolesRepository implements IRolesRepository {
+  constructor( 
+    @inject("PrismaClient") private prisma: PrismaClient
+  ) {
+  }
+
+  public async getByUserId(user_id: string): Promise<Roles[]> {
+    const roles = await this.prisma.roles.findMany({
+      where: {
+        users: {
+          every: {
+            id: user_id
+          }
+        }
+      }
+    })
+
+    return roles
+  }
+}
+
+export default RolesRepository;
